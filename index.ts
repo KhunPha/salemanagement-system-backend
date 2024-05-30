b7ed81ed0567837606c8e40031ac9be14fa28b63
KhunPha <sophadaro2016@gmail.com>
2024-05-30 14:10:43 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response, NextFunction } from "express"
+import { ApolloServer, gql } from "apollo-server-express"
+import cors from "cors"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+const app: any = express()
+
+dotenv.config()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const PORT = process.env.PORT || 3000
+
+const typeDefs = gql`
+    type Query {
+        helloworld: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        helloworld: () => "Bye World"
+    }
+}
+
+app.use("/", (req: Request, res: Response) => {
+    res.json({
+        message: "Welcome to graphql API"
+    })
+})
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const clientIp = req.ip || req.socket.remoteAddress || '127.0.0.1';
+    next();
+});
+
+const startServer = async () => {
+    try {
+        const apolloServer = new ApolloServer({
+            typeDefs,
+            resolvers
+        })
+    
+        await apolloServer.start()
+        apolloServer.applyMiddleware({app, cors: true})
+    
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}${apolloServer.graphqlPath}`)
+        })
+    } catch (error: any) {
+        console.log(error.message)
+    }
+}
+
+startServer()

4820e02fe6c6bac7cbe9ce0f1b4c319b7cde2be3
KhunPha <sophadaro2016@gmail.com>
2024-06-19 09:36:54 +0700

    update 19-06-2024

--- src/schema/sale/sales.schema.ts
+++ src/schema/sale/sales.schema.ts
@@ -1,14 +1,21 @@
-import mongoose, {Schema, Document, mongo} from "mongoose";
+import mongoose, {Schema, Document, mongo, Date} from "mongoose";
 
 export interface ISales extends Document {
     product_lists: object
     cashier: string
+    customer: object
+    paymethod: string
     total_qty: number
     total_amount: number
-    discount_type: string,
+    exchange_rate: number
+    discount_type: string
     product_add: object
     discount: number
     unit_product_discount: object
+    remind_status: boolean
+    date_remind: Date,
+    pay: object
+    bank: object
 }
 
 const sale = new Schema<ISales>({
@@ -27,10 +34,20 @@ const sale = new Schema<ISales>({
     cashier: {
         type: String
     },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Customer"
+    },
+    paymethod: {
+        type: String
+    },
     total_qty: {
         type: Number
     },
     total_amount: {
+        type: Number
+    },
+    exchange_rate: {
         type: Number
     },
     discount_type: {
@@ -59,7 +76,24 @@ const sale = new Schema<ISales>({
         discount: {
             type: Number
         }
-    }]
+    }],
+    remind_status: {
+        type: Boolean
+    },
+    date_remind: {
+        type: Date
+    },
+    pay: {
+        reil: {
+            type: Number
+        },
+        dollar: {
+            type: Number
+        }
+    },
+    bank: {
+        type: mongoose.Schema.Types.ObjectId
+    }
 }, {timestamps: true})
 
 const SaleSchema = mongoose.model<ISales>("Sale", sale, "Sales")

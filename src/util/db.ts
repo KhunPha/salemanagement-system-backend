8e20ef0d851f3b32f94c8064a43871ae3df8ef86
KhunPha <sophadaro2016@gmail.com>
2024-06-21 11:03:35 +0700

    update

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,8 +1,16 @@
 import mongoose from "mongoose"
 import { MONGO_URI } from "../.."
+import { ApolloError } from "apollo-server-express"
 const { success, error } = require("consola")
-mongoose.connect(MONGO_URI)
-success({
-    badge: true,
-    message: `${MONGO_URI}`
-})
+try {
+    mongoose.connect(MONGO_URI)
+    success({
+        badge: true,
+        message: `${MONGO_URI}`
+    })
+} catch (error: any) {
+    error({
+        badge: true,
+        message: error.message
+    })
+}

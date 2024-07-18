ea7497ba4679a4f587f5270936dc17d00140c785
SophaEderson <78026058+KhunPha@users.noreply.github.com>
2024-07-18 18:31:16 +0700

    Update selectData.resolver.ts

--- src/graphql/resolvers/selects/selectData.resolver.ts
+++ src/graphql/resolvers/selects/selectData.resolver.ts
@@ -1,14 +1,16 @@
 import BankSchema from "../../../schema/setting/bank.schema";
 import CategoriesSchema from "../../../schema/setting/categories.schema";
 import ColorSchema from "../../../schema/setting/color.schema";
-import UnitSchema from "../../../schema/setting/unit.shema";
+import UnitSchema from "../../../schema/setting/unit.schema";
+import BrandSchema from "../../../schema/setting/brand.schema;
 
 const selectData = {
     Query: {
         selectUnit: async () => await UnitSchema.find(),
         selectBank: async () => await BankSchema.find(),
         selectCategory: async () => await CategoriesSchema.find(),
-        selectColor: async () => await ColorSchema.find()
+        selectColor: async () => await ColorSchema.find(),
+        selectBrand: async () => await BrandSchema.find()
     }
 }
 

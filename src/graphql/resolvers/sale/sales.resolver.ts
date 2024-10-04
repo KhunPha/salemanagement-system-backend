ba694ec16fa6f575318b91b8e10f9d4996d62913
KhunPha <sophadaro2016@gmail.com>
2024-10-04 11:57:35 +0700

    update

--- src/graphql/resolvers/sale/sales.resolver.ts
+++ src/graphql/resolvers/sale/sales.resolver.ts
@@ -198,6 +198,7 @@ const sales = {
                     total_pay,
                     paid_dollar,
                     paid_riel,
+                    payback,
                     cashier: userToken.data.user.firstname + userToken.data.user.lastname,
                     createdBy: userToken.data.user._id,
                     modifiedBy: userToken.data.user._id
@@ -261,7 +262,7 @@ const sales = {
                     payback = args.input.payback.dollar;
                 }
 
-                let due = findSale?.total_amount - findSale?.total_pay + args.input.pay.dollar + payback;
+                let due = findSale?.total_amount - (findSale?.total_pay + args.input.pay.dollar + payback);
                 let total_pay = findSale?.total_pay + args.input.pay.dollar - payback;
 
                 // Paid
@@ -273,7 +274,7 @@ const sales = {
                 }
 
                 if (args.input.pay.reil > 0) {
-                    due = findSale?.total_amount - findSale?.total_pay + ((args.input.pay.reil / args.input.exchange_rate) + args.input.pay.dollar) + payback;
+                    due = findSale?.total_amount - (findSale?.total_pay + ((args.input.pay.reil / args.input.exchange_rate) + args.input.pay.dollar) + payback);
                     total_pay = findSale?.total_pay + ((args.input.pay.reil / args.input.exchange_rate) + args.input.pay.dollar) - payback;
                     paid_riel += args.input.pay.reil;
                 }

e1bf428e34a1e123378ef2742270d5de23cbba66
KhunPha <sophadaro2016@gmail.com>
2024-10-09 23:33:44 +0700

    update

--- src/graphql/resolvers/stock/paymentTransacPur.resolver.ts
+++ src/graphql/resolvers/stock/paymentTransacPur.resolver.ts
@@ -40,7 +40,7 @@ const paymentTransacPur = {
             try {
                 const { payment_id } = args
 
-                const findPurPayment: any = await PaymentTransacPurSchema.findById(payment_id);
+                const findPurPayment: any = await PaymentTransacPurSchema.findByIdAndUpdate(payment_id, { $set: { isVoid: true } });
                 const findPurchase: any = await PurchaseSchema.findById(findPurPayment?.purchase_id);
 
                 const updateDoc = { $set: { due: findPurPayment?.dollar + findPurchase?.due, total_pay: findPurchase?.total_pay - findPurPayment?.dollar } };
